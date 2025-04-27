4ab4d8a4f81e16d98e7e6a815927ccc652f7a687
justo-creator <justobello352@gmail.com>
2025-04-26 17:45:47 -1200

    el harea de sobre nosotros esta echa

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const toggleButton = document.getElementById('menu-toggle');
+    const navList = document.getElementById('nav-list');
+
+    toggleButton.addEventListener('click', () => {
+        navList.classList.toggle('active');
+        toggleButton.classList.toggle('active')
+    });
