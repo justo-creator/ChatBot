7c21d0015b267f5a430b478b11f35bf886f5598b
justo-creator <justobello352@gmail.com>
2025-05-14 13:21:36 -0700

    validacion del los formulario  e intento conectar el front con el bnack con node

--- js/script.js
+++
@@ -1,8 +0,0 @@
-const toggleButton = document.getElementById('menu-toggle');
-    const navList = document.getElementById('nav-list');
-
-    toggleButton.addEventListener('click', () => {
-        navList.classList.toggle('active');
-        toggleButton.classList.toggle('active')
-    });
-
